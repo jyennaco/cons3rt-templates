492f60ab4ff7f577a05134e2e4ba24ffc2393617
Joe Yennaco <joe@joeyennaco.com>
2020-02-04 13:37:54 -0500

    Updated the service principal creation script to allow for AD user login

--- azure/create_azure_cloud.sh
+++ azure/create_azure_cloud.sh
@@ -3,6 +3,7 @@
 subscriptionId="${1}"
 appName="${2}"
 appUrl="${3}"
+tenantId="${4}"
 
 echo "Let's create an App Registration for CONS3RT!"
 
@@ -15,6 +16,9 @@ fi
 if [ -z "${appUrl}" ]; then
     read -p 'App URL: ' appUrl
 fi
+if [ -z "${tenantId}" ]; then
+    read -p 'Tenant ID (type "skip" if not using multi-tenant login):' tenantId
+fi
 
 echo "Using subscription ID: ${subscriptionId}"
 echo "Creating App Regiatration with name [${appName}] and URL [${appUrl}]"
@@ -22,67 +26,76 @@ echo "Creating App Regiatration with name [${appName}] and URL [${appUrl}]"
 echo "Setting cloud environment to: AzureUSGovernment"
 az cloud set --name AzureUSGovernment
 
-echo "Logging in to Azure (follow instructions)..."
-az login
+if [[ ${tenantId} == "skip" ]]; then
+    echo "Logging in to Azure (follow instructions)..."
+    az login
+else
+    echo "Logging in to Azure using the provided tenant ID (follow instructions)..."
+    az login --tenant="${tenantId}"
+fi
 
 echo "Setting subscription to: ${subscriptionId}"
 az account set --subscription ${subscriptionId}
 if [ $? -ne 0 ]; then echo "ERROR: Setting subscription to: ${subscriptionId}"; exit 1; fi
 
-echo "Getting the AD tenant ID..."
-tenantId=$(az account show --subscription ${subscriptionId} --output yaml | grep 'tenantId' | awk '{print $2}')
-if [ -z "${tenantId}" ]; then echo "AD Tenant ID not found!"; exit 1; fi
+if [[ ${tenant} != "skip" ]]; then
+    echo "Getting the AD tenant ID..."
+    tenantId=$(az account show --subscription ${subscriptionId} --output yaml | grep 'tenantId' | awk '{print $2}')
+    if [ -z "${tenantId}" ]; then echo "AD Tenant ID not found!"; exit 1; fi
+fi
 
 echo "Using AD Tenant ID: ${tenantId}"
 
 echo "Getting this account service principal name..."
 userPrincipalName=$(az ad signed-in-user show --output yaml | grep 'userPrincipalName' | awk '{print $2}')
-if [ -z "${userPrincipalName}" ]; then echo "Service principal name not found!"; exit 1; fi
-
-echo "Found Service Principal Name, running as: ${userPrincipalName}"
-
-echo "Get a list of groups for user: ${userPrincipalName}..."
-groups=( $(az ad user get-member-groups --upn-or-object-id ${userPrincipalName} | awk '{print $1}') )
-echo "Found groups: ${groups[@]}"
-
-echo "Ensure user ${userPrincipalName} has subscription owner permissions or a group does..."
-subscriptionRoleVerified=0
-subscriptionRole=$(az role assignment list --include-inherited --assignee "${userPrincipalName}" --output table | grep "/subscriptions/${subscriptionId}" | awk '{print $2}')
-
-if [ -z "${subscriptionRole}" ]; then
-    echo "Subscription role not found for user: ${userPrincipalName}"
+if [ -z "${userPrincipalName}" ]; then
+    echo "Service principal name not found, assuming AD user with proper permissions is logged in..."
 else
-    echo "Found subscription role for ${userPrincipalName}: ${subscriptionRole}"
-    if [[ ${subscriptionRole} != "Owner" ]]; then
-        echo "ERROR: Requires Subscription Owner, found user permission: ${subscriptionRole}"
+    echo "Found Service Principal Name, running as: ${userPrincipalName}"
+
+    echo "Get a list of groups for user: ${userPrincipalName}..."
+    groups=( $(az ad user get-member-groups --upn-or-object-id ${userPrincipalName} | awk '{print $1}') )
+    echo "Found groups: ${groups[@]}"
+
+    echo "Ensure user ${userPrincipalName} has subscription owner permissions or a group does..."
+    subscriptionRoleVerified=0
+    subscriptionRole=$(az role assignment list --include-inherited --assignee "${userPrincipalName}" --output table | grep "/subscriptions/${subscriptionId}" | awk '{print $2}')
+
+    if [ -z "${subscriptionRole}" ]; then
+        echo "Subscription role not found for user: ${userPrincipalName}"
     else
-        echo "User ${userPrincipalName} has the proper Subscription role!"
-        subscriptionRoleVerified=1
-    fi
-fi
-
-if [ ${subscriptionRoleVerified} -eq 0 ]; then
-    echo "Checking group permissions..."
-    for group in "${groups[@]}"; do
-        echo "Checking permissions for group: ${group}..."
-        subscriptionRole=$(az role assignment list --include-inherited --all --output table | grep "${group}" | grep -v 'Principal' | grep "/subscriptions/${subscriptionId}" | awk '{print $2}')
-        if [ -z "${subscriptionRole}" ]; then
-            echo "No subscription permissions found for group: ${group}"
+        echo "Found subscription role for ${userPrincipalName}: ${subscriptionRole}"
+        if [[ ${subscriptionRole} != "Owner" ]]; then
+            echo "ERROR: Requires Subscription Owner, found user permission: ${subscriptionRole}"
         else
-            echo "Found subscription permission for group ${group}: ${subscriptionRole}"
-            if [[ ${subscriptionRole} != "Owner" ]]; then
-                echo "Requires Subscription Owner, found group permission: ${subscriptionRole}"
-            else
-                echo "Group ${group} has the proper Subscription role!"
-                subscriptionRoleVerified=1
-            fi
+            echo "User ${userPrincipalName} has the proper Subscription role!"
+            subscriptionRoleVerified=1
         fi
-    done
-fi
+    fi
 
-if [ ${subscriptionRoleVerified} -eq 0 ]; then
-    echo "ERROR: Subscription Owner permissions not found, cannot set up the App Registration permissions properly"
-    exit 1
+    if [ ${subscriptionRoleVerified} -eq 0 ]; then
+        echo "Checking group permissions..."
+        for group in "${groups[@]}"; do
+            echo "Checking permissions for group: ${group}..."
+            subscriptionRole=$(az role assignment list --include-inherited --all --output table | grep "${group}" | grep -v 'Principal' | grep "/subscriptions/${subscriptionId}" | awk '{print $2}')
+            if [ -z "${subscriptionRole}" ]; then
+                echo "No subscription permissions found for group: ${group}"
+            else
+                echo "Found subscription permission for group ${group}: ${subscriptionRole}"
+                if [[ ${subscriptionRole} != "Owner" ]]; then
+                    echo "Requires Subscription Owner, found group permission: ${subscriptionRole}"
+                else
+                    echo "Group ${group} has the proper Subscription role!"
+                    subscriptionRoleVerified=1
+                fi
+            fi
+        done
+    fi
+
+    if [ ${subscriptionRoleVerified} -eq 0 ]; then
+        echo "ERROR: Subscription Owner permissions not found, cannot set up the App Registration permissions properly"
+        exit 1
+    fi
 fi
 
 echo "Creating app registration with name [${appName}] and URL [${appUrl}]..."
@@ -120,9 +133,6 @@ echo "Subscription ID: ${subscriptionId}"
 echo "Secret Key: ${appPassword}"
 echo "Tenant Name: ${tenantId}"
 echo "--------------------------"
-
-echo "The following IP addresses found to add to the optional External IP Addresses: "
-az network public-ip list --output table
 
 echo "Happy CONS3RTing!"
 exit 0
